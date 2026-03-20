9e0bd9c066eb928e1bbf49d5ddb1ba3103455a7e
Anuakhi20004 <anusha001.mscds24@nehrucolleges.com>
2026-03-20 21:09:37 +0530

    Create migration for permissions table

---
+++ database/migrations/2024_01_01_000002_create_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function ($table) {
+            $table->id();
+            $table->string('name');
+            $table->string('guard_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
