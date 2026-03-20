17b6d46094f800df9429dd36c3bfc3cbc33f19c5
Anuakhi20004 <anusha001.mscds24@nehrucolleges.com>
2026-03-20 20:56:06 +0530

    Create AuthServiceProvider.php for Auth module

---
+++ app/Modules/Auth/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\\Modules\\Auth\\Providers;
+
+use Illuminate\\Support\\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Set up the application authentication gates.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
